0b42fbdeb546829bf00f6648e9200292e07ea3b9
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2022-07-25 13:58:56 +0700

    refeacter code:create routes, app/controller folder

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+class NewsController {
+  // [GET] /news
+  index(req, res) {
+    res.render("news");
+  }
+  // [GET] /news/:slug
+  show(req, res) {
+    res.send("News Detail !!!");
+  }
+}
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // HTTP Logger
 app.use(morgan("combined"));
@@ -18,12 +23,10 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
-
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
